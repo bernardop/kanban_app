2ac62604c671019cdcb6efcced0399f0a1836e4e
Bernardo Pacheco <berna.ipach@gmail.com>
2015-12-01 17:15:22 -0500

    First commit

---
+++ app/components/App.jsx
@@ -0,0 +1,29 @@
+import AltContainer from 'alt-container';
+import React, {Component} from 'react';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
+
+export default class App extends Component {
+
+
+    render = () => {
+        return (
+            <div>
+                <button className="add-lane" onClick={this.addItem}>+</button>
+                <AltContainer
+                    stores={[LaneStore]}
+                    inject={{
+                        items: () => LaneStore.getState().lanes
+                    }}
+                >
+                    <Lanes />
+                </AltContainer>
+            </div>
+        );
+    }
+
+    addItem = () => {
+        LaneActions.create({name: 'New lane'});
+    }
+}
