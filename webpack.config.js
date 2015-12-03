9f1f614de10b28a7e37c33418d47315ee6a86ec4
Bernardo Pacheco <berna.ipach@gmail.com>
2015-12-03 09:32:00 -0500

    build and deploy configurations

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
+var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pkg = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -18,11 +22,6 @@ var common = {
     },
     module: {
         loaders: [
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: PATHS.app
-            },
             {
                 test: /\.jsx?$/,
                 loaders: ['babel'],
@@ -52,8 +51,56 @@ if (TARGET === 'start' || !TARGET) {
             host: process.env.HOST,
             port: process.env.PORT
         },
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loaders: ['style', 'css'],
+                    include: PATHS.app
+                },
+            ]
+        },
         plugins: [
             new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if (TARGET === 'build' || TARGET === 'stats' || TARGET === 'deploy') {
+    module.exports = merge(common, {
+        entry: {
+            app: PATHS.app,
+            vendor: Object.keys(pkg.dependencies)
+        },
+        output: {
+            path: PATHS.build,
+            filename: '[name].[chunkhash].js'
+        },
+        devtool: 'source-map',
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css'),
+                    include: PATHS.app
+                },
+            ]
+        },
+        plugins: [
+            new Clean(['build']),
+            new ExtractTextPlugin('styles.[chunkhash].css'),
+            new webpack.optimize.CommonsChunkPlugin(
+                'vendor',
+                '[name].[chunkhash].js'
+            ),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
         ]
     });
 }
